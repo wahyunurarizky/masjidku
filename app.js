14f2c9d91eefb633119763304290a39a49ff536f
Wahyu Nur Arizky <arizky.wna@gmail.com>
2021-11-24 21:58:55 +0700

    last

--- app.js
+++ app.js
@@ -19,6 +19,14 @@ const mainViewRouter = require('./routes/mainViewRoutes');
 // *****************************declare app**************************
 const app = express();
 
+if (process.env.NODE_ENV === 'production') {
+  app.use((req, res, next) => {
+    if (req.header('x-forwarded-proto') !== 'https')
+      res.redirect(`https://${req.header('host')}${req.url}`);
+    else next();
+  });
+}
+
 app.use(cors());
 
 app.set('view engine', 'pug');
