52a4c751162ced038a84e24b37c0c6fa72a83615
Wahyu Nur Arizky <arizky.wna@gmail.com>
2021-11-25 01:27:17 +0700

    asdasd

--- controllers/viewAdminControllers.js
+++ controllers/viewAdminControllers.js
@@ -4,7 +4,7 @@ const catchAsync = require('../utils/catchAsync');
 exports.manageMasjids = catchAsync(async (req, res, next) => {
   const masjids = await Masjid.find();
 
-  res.status(200).render('admin/managemasjids.pug', {
+  res.status(200).render('admin/manageMasjids.pug', {
     title: 'kelola masjid',
     masjids,
   });
